858ca4bc0184f5a2487a565383b5c5a62f0f675a
Michael Noguera <michaelnoguera@users.noreply.github.com>
2019-08-24 14:06:52 -0500

    Specify left/right boundaries, increase resoultion of processed video

--- drone_project.py
+++ drone_project.py
@@ -27,8 +27,8 @@ while True:
     frame = drone.video_client.frame
 
     #if frame != last_frame:
-    # Resize frame of video to 1/4 size for faster face recognition processing
-    small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+    # Resize frame of video to 1/2 size for faster face recognition processing
+    small_frame = cv2.resize(frame, (0, 0), fx=0.5, fy=0.5)
 
     # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
     rgb_small_frame = small_frame[:, :, ::-1]
@@ -42,11 +42,11 @@ while True:
 
     # Display the results
     for top, right, bottom, left in face_locations:
-        # Scale back up face locations since the frame we detected in was scaled to 1/4 size
-        top *= 4
-        right *= 4
-        bottom *= 4
-        left *= 4
+        # Scale back up face locations since the frame we detected in was scaled to 1/2 size
+        top *= 2
+        right *= 2
+        bottom *= 2
+        left *= 2
 
         # calculate box size
         height = bottom - top
@@ -59,22 +59,21 @@ while True:
 
         # forward/back control
         cv2.rectangle(frame, (left - 1, bottom + 35), (right + 1, bottom), (255, 0, 0), cv2.FILLED)
-        if (height > 250):
+        if (height > 100):
             cv2.putText(frame, "back up", (left + 6, bottom + 28), font, 1.0, (255, 255, 255), 1)
-        elif (height < 200):
+        elif (height < 75):
             cv2.putText(frame, "move forwards", (left + 6, bottom + 28), font, 1.0, (255, 255, 255), 1)
         else:
             cv2.putText(frame, "stay put", (left + 6, bottom + 28), font, 1.0, (255, 255, 255), 1)
 
         # left/right control
-        # BUG: the proper thresholds are unknown, so it doesn't work
-        #cv2.rectangle(frame, (left - 1, bottom + 70), (right + 1, bottom + 35), (0, 255, 0), cv2.FILLED)
-        #if (too far left):
-        #    cv2.putText(frame, "move right", (left + 6, bottom + 66), font, 1.0, (255, 255, 255), 1)
-        #elif (too far right):
-        #    cv2.putText(frame, "move left", (left + 6, bottom + 66), font, 1.0, (255, 255, 255), 1)
-        #else:
-        #    cv2.putText(frame, "centered", (left + 6, bottom + 66), font, 1.0, (255, 255, 255), 1)
+        cv2.rectangle(frame, (left - 1, bottom + 70), (right + 1, bottom + 35), (0, 255, 0), cv2.FILLED)
+        if (left < 160):
+            cv2.putText(frame, "move left", (left + 6, bottom + 66), font, 1.0, (255, 255, 255), 1)
+        elif (right > 480):
+            cv2.putText(frame, "move right", (left + 6, bottom + 66), font, 1.0, (255, 255, 255), 1)
+        else:
+            cv2.putText(frame, "centered", (left + 6, bottom + 66), font, 1.0, (255, 255, 255), 1)
 
     # Display the resulting image
     cv2.imshow('Video', frame)
